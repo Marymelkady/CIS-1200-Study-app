b7451ca530a57e14aaeab76431d86149c9bb2cbe
Mariem Mohamed El-Kady <101980736+Marymelkady@users.noreply.github.com>
2026-09-05 01:09:20 +0000

    Final Product

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,14 @@ const API = axios.create({
   baseURL: 'https://refactored-yodel-4p97qx57r6pc5qxx-5001.app.github.dev/api',
 });
 
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default API;
